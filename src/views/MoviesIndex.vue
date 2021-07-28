e9fdef0cdaef0958ea28950d38084b8d8dcbbf20
PhoneWalletKeys <rsvetter91@gmail.com>
2021-07-28 09:20:35 -0700

    added a card in my index

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="movies-index">
+<div class="movies-index">
+    <div v-for="movie in movies" :key="movie.id">
+    <div class="row" v-for="movie in movies" :key="movie.id">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <router-link v-bind:to="`/movies/${movie.id}`">
+              <h5 class="card-title">{{ movie.title }}</h5>
+            </router-link>
+            <p class="card-text">{{ movie.year }}</p>
+            <p class="card-text">{{ movie.plot }}</p>
+            <a href="/movies/${movie.id}" class="btn btn-primary">More info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- <div class="movies-index">
     <div v-for="movie in movies" :key="movie.id">
       <router-link v-bind:to="`/movies/#{movie.id}`">
         <h2>Title: {{ movie.title }}</h2>
         <p>Plot: {{ movie.plot }}</p>
       </router-link>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
